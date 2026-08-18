a5bf9a0f09db56e6f7bb43ad5c7e07a56f6d4327
Nghiêm Xuân Bảo <nghiemxuanbaocbq@gmail.com>
2026-08-18 22:37:30 +0700

    hd

--- src/pages/PublicGuide.jsx
+++ src/pages/PublicGuide.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { Link } from 'react-router-dom';
 import { 
   BookOpen, Trophy, HeartHandshake, Mail, Upload, 
   AlertCircle, HelpCircle, Award, ShieldCheck, Sparkles
